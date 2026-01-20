2d628f61b382f43e4af3c218f733864610225772
Dana <k6jq@comcast.net>
2026-01-20 12:27:01 -0800

    Update configuration for minimum power use; SWD disabled by default

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -62,10 +62,12 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PA0 PA1 PA4 PA5
                            PA6 PA7 PA8 PA9
-                           PA10 PA11 PA12 PA15 */
+                           PA10 PA11 PA12 PA13
+                           PA14 PA15 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_5
                           |GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9
-                          |GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_15;
+                          |GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13
+                          |GPIO_PIN_14|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
